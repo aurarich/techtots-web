10d01763c3eff2f4b92f4e7f4cc2ae06b95b0888
codingbrowny <codingbrowny@gmail.com>
2025-07-13 20:38:52 +0000

    config for ftp

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: "export",
+  distDir: "techtots.edu.gh",
   images: {
+    unoptimized: true,
     domains: ['images.unsplash.com', 'plus.unsplash.com', 'www.pexels.com', 'images.pexels.com'],
   },
 }
