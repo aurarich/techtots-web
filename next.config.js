8e9f377055be5786abe6b05ad7515c72e40beebb
codingbrowny <codingbrowny@gmail.com>
2025-07-14 03:40:51 +0000

    global error handling

--- next.config.js
+++ next.config.js
@@ -3,8 +3,25 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['images.unsplash.com', 'plus.unsplash.com', 'www.pexels.com', 'images.pexels.com'],
+    remotePatterns: [
+      // new URL("https://images.unsplash.com"),
+      // new URL("https://plus.unsplash.com"),
+      // new URL("https://www.pexels.com"),
+      // new URL("https://images.pexels.com"),
+      {
+        hostname: "images.pexels.com"
+      },
+      {
+        hostname: "www.pexels.com"
+      },
+      {
+        hostname: "plus.unsplash.com"
+      },
+      {
+        hostname: "images.unsplash.com"
+      },
+    ],
   },
-}
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
