bc68012bce07293f1baadacd7e045d7a5bb2121c
samobeng27 <samobeng23@gmail.com>
2025-08-12 10:29:55 +0000

    pushing to dev

--- src/app/wonderlab/page.tsx
+++ src/app/wonderlab/page.tsx
@@ -10,6 +10,9 @@ type Flight = {
     direction: "top" | "bottom" | "left" | "right";
 };
 
+
+
+
 export default function Page() {
     const [flight, setFlight] = useState<Flight | null>(null);
     const [emit, setEmit] = useState(false); // toggle comet emission while flying
