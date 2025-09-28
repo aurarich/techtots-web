9ad09781707369baab165267c9c3bc8cd9ebaaeb
codingbrowny <codingbrowny@gmail.com>
2025-09-28 19:42:56 +0000

    merge for prod

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  trailingSlash: true,
-  output: isProd ? 'export' : undefined,
-  distDir: isProd ? 'techtots.edu.gh' : '.next',
+  // trailingSlash: true,
+  // output: isProd ? 'export' : undefined,
+  // distDir: isProd ? 'techtots.edu.gh' : '.next',
   images: {
     remotePatterns: [
       { hostname: "images.pexels.com" },
